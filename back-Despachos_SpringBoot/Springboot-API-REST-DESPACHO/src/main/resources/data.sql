325f0eaeb17bff24cb45edd6b889c29f52c0075e
Hans Gómez <han.gomez@duocuc.cl>
2026-06-30 17:39:14 -0400

    fix: use insert ignore to prevent duplicate key errors in pods

--- back-Despachos_SpringBoot/Springboot-API-REST-DESPACHO/src/main/resources/data.sql
+++ back-Despachos_SpringBoot/Springboot-API-REST-DESPACHO/src/main/resources/data.sql
@@ -1,3 +1,3 @@
 -- Poblado inicial para la tabla despacho
-INSERT INTO despacho (id_despacho, fecha_despacho, patente_camion, intento, id_compra, direccion_compra, valor_compra, despachado) 
+INSERT IGNORE INTO despacho (id_despacho, fecha_despacho, patente_camion, intento, id_compra, direccion_compra, valor_compra, despachado) 
 VALUES (1, '2023-11-22', 'AB-CD-12', 1, 2, 'Calle Falsa 123, Valparaíso', 15500, false);
--- back-Ventas_SpringBoot/Springboot-API-REST/src/main/resources/data.sql
+++ back-Ventas_SpringBoot/Springboot-API-REST/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 -- Poblado inicial para la tabla venta
-INSERT INTO venta (id_venta, direccion_compra, valor_compra, fecha_compra, despacho_generado) 
+INSERT IGNORE INTO venta (id_venta, direccion_compra, valor_compra, fecha_compra, despacho_generado) 
 VALUES (1, 'Av. Providencia 1234, Santiago', 45000, '2023-11-20', false);
 
-INSERT INTO venta (id_venta, direccion_compra, valor_compra, fecha_compra, despacho_generado) 
+INSERT IGNORE INTO venta (id_venta, direccion_compra, valor_compra, fecha_compra, despacho_generado) 
 VALUES (2, 'Calle Falsa 123, Valparaíso', 15500, '2023-11-21', true);
